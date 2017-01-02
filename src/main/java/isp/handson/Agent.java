2de967cff0b90d6758ef513254e2a53a6c51aec2
David Jelenc <david.jelenc@fri.uni-lj.si>
2017-01-02 17:49:36 +0100

    Updates the template

--- src/main/java/isp/handson/Agent.java
+++ src/main/java/isp/handson/Agent.java
@@ -1,21 +1,67 @@
 package isp.handson;
 
+import javax.xml.bind.DatatypeConverter;
 import java.security.Key;
 import java.util.concurrent.BlockingQueue;
 
+/**
+ * Represents an agent that can communicate with others using an ideal
+ * (noiseless) communication channel.
+ * <p/>
+ * Agent's behavior is implemented by defining the execute method
+ */
 public abstract class Agent extends Thread {
     protected final BlockingQueue<byte[]> outgoing, incoming;
 
-    protected final Key macKey, cryptoKey;
-    protected final String cryptoAlgorithm, macAlgorithm;
+    protected final Key cipherKey;
+    protected final String cipher;
 
-    public Agent(final BlockingQueue<byte[]> outgoing, final BlockingQueue<byte[]> incoming, final Key cryptoKey,
-            final String cryptoAlgorithm, final Key macKey, final String macAlgorithm) {
+    public Agent(final String name, final BlockingQueue<byte[]> outgoing,
+                 final BlockingQueue<byte[]> incoming, final Key key, final String cipher) {
+        super(name);
         this.outgoing = outgoing;
         this.incoming = incoming;
-        this.cryptoKey = cryptoKey;
-        this.cryptoAlgorithm = cryptoAlgorithm;
-        this.macKey = macKey;
-        this.macAlgorithm = macAlgorithm;
+        this.cipherKey = key;
+        this.cipher = cipher;
+    }
+
+    /**
+     * Converts a byte[] into a string of HEX values
+     *
+     * @param bytes
+     * @return
+     */
+    public String hex(byte[] bytes) {
+        return DatatypeConverter.printHexBinary(bytes);
+    }
+
+    /**
+     * Override this method to define agent's behavior
+     *
+     * @throws Exception
+     */
+    public abstract void execute() throws Exception;
+
+    @Override
+    public void run() {
+        try {
+            execute();
+        } catch (Exception e) {
+            print("EXCEPTION: %s", e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Prints a message to standard output. Follows the same semantics
+     * as {@link System#out#printf}
+     *
+     * @param string
+     * @param obj
+     */
+    public void print(String string, Object... obj) {
+        System.out.printf("[%s] ", getName());
+        System.out.printf(string, obj);
+        System.out.println();
     }
 }
--- src/main/java/isp/handson/HandsOnAssignment.java
+++ src/main/java/isp/handson/HandsOnAssignment.java
@@ -1,52 +1,35 @@
 package isp.handson;
 
-import javax.xml.bind.DatatypeConverter;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
-import java.util.logging.Logger;
 
 public class HandsOnAssignment {
-    private final static Logger LOG = Logger.getLogger(HandsOnAssignment.class.getCanonicalName());
-
     public static void main(String[] args) {
-
         final BlockingQueue<byte[]> alice2bob = new LinkedBlockingQueue<>();
         final BlockingQueue<byte[]> bob2alice = new LinkedBlockingQueue<>();
 
-        final Agent alice = new Agent(alice2bob, bob2alice, null, null, null, null) {
+        final Agent alice = new Agent("alice", alice2bob, bob2alice, null, null) {
             @Override
-            public void run() {
-                try {
-                    final String message = "I love you Bob. Kisses, Alice.";
-                    outgoing.put(message.getBytes("UTF-8"));
-                    System.out.printf("[Alice]: Sending to Bob: %s%n", message);
-                } catch (Exception e) {
-                    System.err.printf("[Alice] Exception: %s%n.", e.getMessage());
-                    e.printStackTrace();
-                }
+            public void execute() throws Exception {
+                final String message = "Hi Bob. Kisses, Alice.";
+                final byte[] bytes = message.getBytes("UTF-8");
+
+                print("Sending: '%s' (HEX: %s)", message, hex(bytes));
+                outgoing.put(bytes);
             }
         };
 
-        final Agent bob = new Agent(bob2alice, alice2bob, null, null, null, null) {
-
+        final Agent bob = new Agent("bob", bob2alice, alice2bob, null, null) {
             @Override
-            public void run() {
-                try {
-                    final byte[] payload = incoming.take();
-                    final String message = new String(payload, "UTF-8");
-                    System.out.printf("[Bob]: I have received '%s' which in UTF-8 is '%s'%n", str(payload), message);
-                } catch (Exception e) {
-                    System.err.printf("[Bob] Exception: %s%n.", e.getMessage());
-                    e.printStackTrace();
-                }
+            public void execute() throws Exception {
+                final byte[] bytes = incoming.take();
+                final String message = new String(bytes, "UTF-8");
+                print("Received: '%s' (HEX: %s)", message, hex(bytes));
             }
         };
 
+        // start both threads
         bob.start();
         alice.start();
-    }
-
-    public static String str(final byte[] data) {
-        return DatatypeConverter.printHexBinary(data);
     }
 }
