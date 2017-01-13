24d3159cc8c4ce2af3d57651b6a6c79e8eab5985
David Jelenc <david.jelenc@fri.uni-lj.si>
2017-01-13 13:36:04 +0100

    Prepared template

--- src/main/java/isp/handson/HandsOnAssignment.java
+++ src/main/java/isp/handson/HandsOnAssignment.java
@@ -4,32 +4,42 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class HandsOnAssignment {
-    public static void main(String[] args) {
-        final BlockingQueue<byte[]> alice2bob = new LinkedBlockingQueue<>();
-        final BlockingQueue<byte[]> bob2alice = new LinkedBlockingQueue<>();
+    public static void main(String[] args) throws Exception {
 
-        final Agent alice = new Agent("alice", alice2bob, bob2alice, null, null) {
+        final BlockingQueue<byte[]> alice2server = new LinkedBlockingQueue<>();
+        final BlockingQueue<byte[]> alice2lock = new LinkedBlockingQueue<>();
+        final BlockingQueue<byte[]> server2lock = new LinkedBlockingQueue<>();
+
+        final Agent alice = new Agent("alice", null, null, null, null) {
             @Override
             public void execute() throws Exception {
-                final String message = "Hi Bob. Kisses, Alice.";
-                final byte[] bytes = message.getBytes("UTF-8");
-
-                print("Sending: '%s' (HEX: %s)", message, hex(bytes));
-                outgoing.put(bytes);
+                alice2server.put("from Alice".getBytes());
+                alice2lock.put("from Alice".getBytes());
             }
         };
 
-        final Agent bob = new Agent("bob", bob2alice, alice2bob, null, null) {
+        final Agent server = new Agent("server", null, null, null, null) {
             @Override
             public void execute() throws Exception {
-                final byte[] bytes = incoming.take();
-                final String message = new String(bytes, "UTF-8");
-                print("Received: '%s' (HEX: %s)", message, hex(bytes));
+                server2lock.put("from Server".getBytes());
+                final byte[] msg = alice2server.take();
+                print("Got %s", new String(msg));
             }
         };
 
-        // start both threads
-        bob.start();
+        final Agent lock = new Agent("lock", null, null, null, null) {
+            @Override
+            public void execute() throws Exception {
+                final byte[] msg1 = alice2lock.take();
+                print("Got %s", new String(msg1));
+
+                final byte[] msg2 = server2lock.take();
+                print("Got %s", new String(msg2));
+            }
+        };
+
         alice.start();
+        server.start();
+        lock.start();
     }
 }
